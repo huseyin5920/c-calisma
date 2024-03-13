76d19b4f59ae25d7b83cc1d2f5f84c53496aa1aa
huseyin.kaya <huseyin.kaya@interprobe.com.tr>
2024-03-14 00:14:17 +0300

    hesap makinesi yapıldı

---
+++ bank_management.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <postgresql/libpq-fe.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+    printf("banka yönetim sistemine hoş geldiniz");
+}
+
---
+++ calculator.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <postgresql/libpq-fe.h>
+#include <stdlib.h>
+
+ 
+int main()
+{
+    int sayi1, sayi2, secim;
+    float sonuc;
+
+    printf("1. sayiyi giriniz: ");
+    scanf("%d", &sayi1);
+    printf("\n2. sayiyi giriniz: ");
+    scanf("%d",&sayi2);
+    printf("\nyapmak istediğiniz işlemi seçiniz: + = 1 , - = 2 , * = 3 , / = 4\n");
+    scanf("%d", &secim);
+
+    switch (secim)
+    {
+    case 1:
+        sonuc = sayi1 + sayi2;
+        printf("\n%f", sonuc);
+        break;
+    case 2:
+        sonuc = sayi1 - sayi2;
+        printf("\n%f", sonuc);
+        break;
+    case 3:
+        sonuc = sayi1 * sayi2;
+        printf("\n%f", sonuc);
+        break;
+    case 4:
+        sonuc = sayi1 / sayi2;
+        printf("\n%f", sonuc);
+        break;    
+    default:
+        printf("geçersiz işlem");
+        break;
+    }
+}
